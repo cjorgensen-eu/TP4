0013de270ce5f9a1664192886538a785306bab14
Carl Jorgensen <carl@cjorgensen.eu>
2021-01-19 15:22:25 +0100

    reset d'anciens commits + ajout d'un commentaire dans un script

--- list_groups.sh
+++ list_groups.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# ce script liste tous les groupes du systeme
+
 cut -d: -f1 /etc/group
--- list_users.sh
+++ list_users.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+# ce script liste tous les utilisateurs du systeme
+
 cut -d: -f1 /etc/passwd
-dzgfqefghef
